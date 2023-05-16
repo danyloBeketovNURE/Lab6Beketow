73c4fe1e41def44f46f33d8538608e4491a8f4d9
Ivan Shevelyn <ivan.shevelyn@nure.ua>
2023-05-16 16:49:17 +0300

    Зміни в коді хоста

---
+++ Host/MyBehavior.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Description;
+using System.ServiceModel.Dispatcher;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Host
+{
+    public class MyBehavior : IEndpointBehavior
+    {
+        public void AddBindingParameters(ServiceEndpoint endpoint, BindingParameterCollection bindingParameters)
+        {
+            
+        }
+
+        public void ApplyClientBehavior(ServiceEndpoint endpoint, ClientRuntime clientRuntime)
+        {
+            
+        }
+
+        public void ApplyDispatchBehavior(ServiceEndpoint endpoint, EndpointDispatcher endpointDispatcher)
+        {
+            endpointDispatcher.DispatchRuntime.AutomaticInputSessionShutdown = false;
+        }
+
+        public void Validate(ServiceEndpoint endpoint)
+        {
+            
+        }
+    }
+}
--- Host/Program.cs
+++ Host/Program.cs
@@ -16,6 +16,8 @@ namespace Host
             using (ServiceHost serviceHost = new ServiceHost(typeof(ServiceBank)))
             {
                 serviceHost.Open();
+                foreach (var endPoint in serviceHost.Description.Endpoints)
+                    endPoint.EndpointBehaviors.Add(new MyBehavior());
                 Console.WriteLine("The service is ready.");
                 Console.WriteLine("Press the Enter key to terminate service.");
                 Console.ReadLine();
